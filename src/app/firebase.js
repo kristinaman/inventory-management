66c37f4a4041c5dd0d307c3a130bbef7a029818b
kristinaman <kristina.kw.man@gmail.com>
2024-08-07 00:28:56 -0400

    cleaned

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCY10pnJbIH9Ox_tTfKDmInrKDOnRoyw1g",
-  authDomain: "inventory-management-app-238b4.firebaseapp.com",
-  projectId: "inventory-management-app-238b4",
-  storageBucket: "inventory-management-app-238b4.appspot.com",
-  messagingSenderId: "782956092129",
-  appId: "1:782956092129:web:3c09dba0be55e1a6ea35a0",
-  measurementId: "G-9H3DTEGN8K",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementID: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
